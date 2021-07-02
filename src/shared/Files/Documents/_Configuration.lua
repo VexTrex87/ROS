7741fc0cad3b2e6adfbde7ec7c6223ad30547438
Vex87 <AndrewKyleGuilas@gmail.com>
2021-07-02 16:10:51 -0400

    Added config for libraries

---
+++ src/shared/Files/Documents/_Configuration.lua
@@ -0,0 +1,6 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Assets = require(ReplicatedStorage.Configuration.Assets)
+
+return {
+    Icon = Assets["Document_Type_Icon"]
+}
---
+++ src/shared/Files/Videos/_Configuration.lua
@@ -0,0 +1,6 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Assets = require(ReplicatedStorage.Configuration.Assets)
+
+return {
+    Icon = Assets["Video_Type_Icon"]
+}
